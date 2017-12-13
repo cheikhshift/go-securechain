74c5e439f524903b292ee9dd7b551b8af7765021
Cheikh Seck <jvmstix@gmail.com>
2017-12-13 01:55:13 +0000

    Add files via upload

--- securechain_test.go
+++ securechain_test.go
@@ -3,8 +3,47 @@ package securechain
 import (
 	"github.com/go-test/deep"
 	"gopkg.in/mgo.v2/bson"
+	"io/ioutil"
+	"net/http"
+	"strings"
 	"testing"
 )
+
+func TestSave(t *testing.T) {
+
+	expgen00 := Save(Mockname, bson.M{})
+	if diff := deep.Equal(expgen00, nil); diff != nil {
+		t.Error(diff)
+	}
+
+}
+
+func BenchmarkSaveMocknamebsonM(b *testing.B) {
+	// credits to @davecheney
+	for n := 0; n < b.N; n++ {
+		Save(Mockname, bson.M{})
+	}
+}
+
+func TestLoad(t *testing.T) {
+
+	expgen00, expgen01 := Load(Mockname)
+	if diff := deep.Equal(expgen00, bson.M{}); diff != nil {
+		t.Error(diff)
+	}
+
+	if diff := deep.Equal(expgen01, nil); diff != nil {
+		t.Error(diff)
+	}
+
+}
+
+func BenchmarkLoadMockname(b *testing.B) {
+	// credits to @davecheney
+	for n := 0; n < b.N; n++ {
+		Load(Mockname)
+	}
+}
 
 func TestToString(t *testing.T) {
 
@@ -58,6 +97,22 @@ func BenchmarkLogbyte(b *testing.B) {
 	}
 }
 
+func TestReadBody(t *testing.T) {
+
+	expgen00 := ReadBody(&http.Response{Body: ioutil.NopCloser(strings.NewReader("test"))})
+	if diff := deep.Equal(expgen00, []byte("test")); diff != nil {
+		t.Error(diff)
+	}
+
+}
+
+func BenchmarkReadBodyhttpResponseBodyioutilNopCloserstringsNewReadertest(b *testing.B) {
+	// credits to @davecheney
+	for n := 0; n < b.N; n++ {
+		ReadBody(&http.Response{Body: ioutil.NopCloser(strings.NewReader("test"))})
+	}
+}
+
 func TestEndpoint(t *testing.T) {
 
 	expgen00 := Endpoint("Login")
@@ -87,41 +142,5 @@ func BenchmarkWrapReqbsonM(b *testing.B) {
 	// credits to @davecheney
 	for n := 0; n < b.N; n++ {
 		WrapReq(bson.M{})
-	}
-}
-
-func TestSave(t *testing.T) {
-
-	expgen00 := Save(Mockfilename(), bson.M{})
-	if diff := deep.Equal(expgen00, nil); diff != nil {
-		t.Error(diff)
-	}
-
-}
-
-func BenchmarkSaveMockfilenamebsonM(b *testing.B) {
-	// credits to @davecheney
-	for n := 0; n < b.N; n++ {
-		Save(Mockfilename(), bson.M{})
-	}
-}
-
-func TestLoad(t *testing.T) {
-
-	expgen00, expgen01 := Load(Mockfilename())
-	if diff := deep.Equal(expgen00, bson.M{}); diff != nil {
-		t.Error(diff)
-	}
-
-	if diff := deep.Equal(expgen01, nil); diff != nil {
-		t.Error(diff)
-	}
-
-}
-
-func BenchmarkLoadMockfilename(b *testing.B) {
-	// credits to @davecheney
-	for n := 0; n < b.N; n++ {
-		Load(Mockfilename())
 	}
 }
